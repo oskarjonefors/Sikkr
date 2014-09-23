c27ef78a3c2680162e429381d3769dd7dee6f4ff
Oskar Jönefors <oskar.jonefors@gmail.com>
2014-09-23 10:42:53 +0200

    Fixed imports in interfaces.

--- app/src/main/java/edu/chalmers/sikkr/backend/Contact.java
+++ app/src/main/java/edu/chalmers/sikkr/backend/Contact.java
@@ -1,5 +1,7 @@
 package edu.chalmers.sikkr.backend;
 
+import java.util.List;
+
 /**
  * Interface representing a contact.
  */
--- app/src/main/java/edu/chalmers/sikkr/backend/ContactBook.java
+++ app/src/main/java/edu/chalmers/sikkr/backend/ContactBook.java
@@ -1,5 +1,7 @@
 package edu.chalmers.sikkr.backend;
 
+import java.util.List;
+
 /**
  * @author Oskar Jönefors
  */
@@ -8,7 +10,7 @@ public interface ContactBook {
     /**
      * Get a list of all the contacts' initial letters.
      */
-    List<char> getInitialLetters();
+    List<Character> getInitialLetters();
 
     /**
      * Get a list of all the contacts in the contact book.
