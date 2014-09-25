b9e8af71e6dcadbf687a3e5cbc382761b3695669
Eric Bjuhr <Sweed.Raver@gmail.com>
2014-09-25 15:16:41 +0200

    Created a utility class for TextToSpeech

---
+++ app/src/main/java/edu/chalmers/sikkr/backend/util/TextToSpeechUtility.java
@@ -0,0 +1,42 @@
+package edu.chalmers.sikkr.backend.util;
+
+import android.content.Context;
+import android.speech.tts.TextToSpeech;
+
+/**
+ * Created by Eric on 2014-09-25.
+ */
+public final class TextToSpeechUtility {
+
+    private static TextToSpeech tts;
+    private final static SetupListener setupListener = new SetupListener();
+
+    private TextToSpeechUtility() {
+        throw new UnsupportedOperationException("Cannot create instance");
+    }
+
+    public final void setupTextToSpeech(Context context) {
+        tts = new TextToSpeech(context, setupListener);
+    }
+
+    public final static void readAloud(String msg, Context context) {
+        if (tts != null) {
+            tts.speak(msg, TextToSpeech.QUEUE_FLUSH, null);
+        }
+    }
+
+    private final static void removeTextToSpeech() {
+        tts = null;
+    }
+
+    private final static class SetupListener implements TextToSpeech.OnInitListener {
+
+        @Override
+        public void onInit(int status) {
+            if (status == TextToSpeech.ERROR) {
+                removeTextToSpeech();
+            }
+        }
+    }
+
+}
