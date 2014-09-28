462d50ee64de33ac09a30525c7e9cddcf9397599
Oskar Jönefors <oskar.jonefors@gmail.com>
2014-09-28 20:12:57 +0200

    Updated Contact interface to define contact photo methods.

--- app/src/main/java/edu/chalmers/sikkr/backend/Contact.java
+++ app/src/main/java/edu/chalmers/sikkr/backend/Contact.java
@@ -1,11 +1,23 @@
 package edu.chalmers.sikkr.backend;
 
+import android.graphics.Bitmap;
+
 import java.util.List;
 
 /**
  * Interface representing a contact.
  */
 public interface Contact extends Comparable<Contact> {
+
+    /**
+     * Returns the thumbnail-sized contact photo. If no such photo exists, an empty Bitmap is returned.
+     */
+    Bitmap getThumbnail();
+
+    /**
+     * Returns the full-sized contact photo. If no such photo exists, an empty Bitmap is returned.
+     */
+    Bitmap getPhoto();
 
     /**
      * Returns the internal representation of the contact.
