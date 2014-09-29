e3b5058cad39e3a827a39b4ffc3e19c8c6c5d9d7
Emilia Nilsson <eminil@student.chalmers.se>
2014-09-29 19:37:25 +0200

    Made the picture change to the contacts picture

--- app/src/main/java/edu/chalmers/sikkr/ContactActivity.java
+++ app/src/main/java/edu/chalmers/sikkr/ContactActivity.java
@@ -12,17 +12,22 @@ import android.widget.TextView;
 import android.net.Uri;
 import android.util.Log;
 
+import java.util.List;
+
 import edu.chalmers.sikkr.backend.Contact;
 import edu.chalmers.sikkr.backend.ContactBook;
 
 
 public class ContactActivity extends Activity {
 
+    private Contact contact;
+
     public void buttonClick(View view) {
         //Brings out the phone dialer
         Intent phoneIntent = new Intent(Intent.ACTION_CALL);
-        //sets the data for which number to call, in this case Joel
-        phoneIntent.setData(Uri.parse("tel:0736958002"));
+
+        //Sets the data for which number to call
+        phoneIntent.setData(Uri.parse("0736958002"));
         try{
             startActivity(phoneIntent);
             finish();
@@ -37,15 +42,23 @@ public class ContactActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
 
+        //This makes us able to use the TextView defined in the .xml and change it from here
         final TextView contactName = (TextView)findViewById(R.id.contactName);
         final ImageView contactPicture = (ImageView)findViewById(R.id.contactPicture);
 
+        //Gets the shared instance ContactBook
         final ContactBook book = ContactBook.getSharedInstance();
+        //Saves the content of the intent in a bundle
         final Bundle bundle = getIntent().getExtras();
-        final Contact contact = book.getContact(bundle.getString("contact_id"));
 
+        //From the bundle we get the contact ID,
+        //and with it we get the right contact from the Contact Book
+        contact = book.getContact(bundle.getString("contact_id"));
+
+        //Set the name of the contact as the text in the TextView
         contactName.setText(contact.getName());
-
+        //Set the picture of the contact in the ImageView
+        contactPicture.setImageBitmap(contact.getPhoto());
     }
 
 
