2c87db14200de4bb9f0353b44731ed928257fa2f
Oskar Jönefors <oskar.jonefors@gmail.com>
2014-09-29 17:06:39 +0200

    Cleaned up ContactBookActivity.

--- app/src/main/java/edu/chalmers/sikkr/ContactBookActivity.java
+++ app/src/main/java/edu/chalmers/sikkr/ContactBookActivity.java
@@ -1,32 +1,13 @@
 package edu.chalmers.sikkr;
 
         import android.app.Activity;
-        import android.content.Context;
-        import android.content.Intent;
-        import android.graphics.Color;
         import android.os.Bundle;
         import android.view.Menu;
         import android.view.MenuItem;
-        import android.view.View;
-        import android.view.ViewGroup;
-        import android.widget.AdapterView;
-        import android.widget.BaseAdapter;
-        import android.widget.Button;
         import android.widget.GridView;
-        import android.widget.ImageView;
-        import android.widget.ListAdapter;
-        import android.widget.Toast;
-
-        import java.util.ArrayList;
-        import java.util.Set;
-
-        import edu.chalmers.sikkr.backend.ContactBook;
 
 
 public class ContactBookActivity extends Activity {
-    private ContactBook cb;
-    private ArrayList<Character> al;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,11 +33,7 @@ public class ContactBookActivity extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        return item.getItemId() == R.id.action_settings || super.onOptionsItemSelected(item);
     }
 
 
