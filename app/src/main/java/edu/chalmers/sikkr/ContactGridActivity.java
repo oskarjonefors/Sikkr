84561348f1a2cb8138737244fce93b87ce1b0977
Oskar Jönefors <oskar.jonefors@gmail.com>
2014-09-29 17:12:47 +0200

    Cleaned up ContactGridActivity.

--- app/src/main/java/edu/chalmers/sikkr/ContactGridActivity.java
+++ app/src/main/java/edu/chalmers/sikkr/ContactGridActivity.java
@@ -55,10 +55,6 @@ public class ContactGridActivity extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        return item.getItemId() == R.id.action_settings || super.onOptionsItemSelected(item);
     }
 }
