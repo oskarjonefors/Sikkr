1be36350b4c7bfb8372033e74309dda31561ff1e
Oskar Jönefors <oskar.jonefors@gmail.com>
2014-10-02 16:30:54 +0200

    Added a voice message player.

---
+++ app/src/main/java/edu/chalmers/sikkr/backend/util/VoiceMessagePlayer.java
@@ -0,0 +1,65 @@
+package edu.chalmers.sikkr.backend.util;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.util.Log;
+
+import java.io.IOException;
+
+import edu.chalmers.sikkr.backend.VoiceMessage;
+
+/**
+ * Created by ivaldi on 2014-10-02.
+ */
+public class VoiceMessagePlayer {
+
+    private String TAG = "VoiceMessagePlayer";
+    private final static VoiceMessagePlayer singleton = new VoiceMessagePlayer();
+    private Context context;
+    private MediaPlayer player;
+
+    private VoiceMessagePlayer() {}
+
+    public VoiceMessagePlayer getSharedInstance() {
+        if(context == null) {
+            throw new UnsupportedOperationException("Context must be supplied through the method" +
+                    "setupSingleton(Context context) before an instance can be returned.");
+        } else {
+            return singleton;
+        }
+    }
+
+    private void setup(Context context) {
+        this.context = context;
+    }
+
+    public void setupSingleton(Context context) {
+        if(context == null) {
+            throw new IllegalArgumentException("Context cannot be null.");
+        } else {
+            this.context = context;
+        }
+    }
+
+    public void playMessage(VoiceMessage msg) {
+        if(player != null) {
+            player.release();
+            player = null;
+        }
+        player = new MediaPlayer();
+
+        try {
+            player.setDataSource(msg.getFilePath());
+            player.prepare();
+            player.start();
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to prepare MediaPlayer.");
+        }
+    }
+
+    public void stop() {
+        if(player != null) {
+            player.stop();
+        }
+    }
+}
