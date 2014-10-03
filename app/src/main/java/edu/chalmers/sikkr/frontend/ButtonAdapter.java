f6c5d6420bde1a27aa7caec51005a512dfdc9501
Jesper Olsson <jespols@student.chalmers.se>
2014-10-03 13:59:40 +0200

    Voice recognition now works for contact book as well.

--- app/src/main/java/edu/chalmers/sikkr/frontend/ButtonAdapter.java
+++ app/src/main/java/edu/chalmers/sikkr/frontend/ButtonAdapter.java
@@ -1,12 +1,14 @@
 package edu.chalmers.sikkr.frontend;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -18,6 +20,7 @@ import edu.chalmers.sikkr.backend.contact.ContactBook;
  */
 public class ButtonAdapter extends BaseAdapter {
     private Context mContext;
+    private Character mLetter;
     final private ArrayList<Character> al;
 
     public ButtonAdapter(Context c) {
@@ -67,6 +70,8 @@ public class ButtonAdapter extends BaseAdapter {
 
         return btn;
     }
+
+
 
 
 
--- app/src/main/java/edu/chalmers/sikkr/frontend/ContactBookActivity.java
+++ app/src/main/java/edu/chalmers/sikkr/frontend/ContactBookActivity.java
@@ -1,29 +1,32 @@
 package edu.chalmers.sikkr.frontend;
 
         import android.app.Activity;
+        import android.content.Intent;
         import android.os.Bundle;
+        import android.speech.RecognizerIntent;
         import android.view.Menu;
         import android.view.MenuItem;
         import android.view.Window;
         import android.widget.GridView;
 
+        import java.util.ArrayList;
+
         import edu.chalmers.sikkr.R;
+        import edu.chalmers.sikkr.backend.util.SpeechRecognitionHelper;
+        import edu.chalmers.sikkr.backend.util.SystemData;
 
 
 public class ContactBookActivity extends Activity {
-
-    //TODO - Implement voice control for this activity as well
+    private ArrayList<String> matches;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_contact_book);
-
-
-
         GridView gridview = (GridView) findViewById(R.id.gridview);
         gridview.setAdapter(new ButtonAdapter(this));
+        SpeechRecognitionHelper.run(this);
     }
 
 
@@ -40,6 +43,18 @@ public class ContactBookActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return item.getItemId() == R.id.action_settings || super.onOptionsItemSelected(item);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == SystemData.VOICE_RECOGNITION_REQUEST_CODE && resultCode == RESULT_OK) {
+            matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            if (matches.size() > 0) {
+                Intent intent = new Intent(this, ContactGridActivity.class);
+                intent.putExtra("initial_letter", matches.get(0).charAt(0));
+                startActivity(intent);
+
+            }
+        }
     }
 
 
