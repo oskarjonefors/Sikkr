99be11e426a0f4a441771272deaf53c93681e04e
Jesper Olsson <jespols@student.chalmers.se>
2014-10-03 12:35:18 +0200

    Minor fixes in speechrecognitionhelper

--- app/src/main/java/edu/chalmers/sikkr/backend/util/SpeechRecognitionHelper.java
+++ app/src/main/java/edu/chalmers/sikkr/backend/util/SpeechRecognitionHelper.java
@@ -10,15 +10,20 @@ import java.util.List;
 
 public class SpeechRecognitionHelper{
 
+    //TODO - installGoogleMethod shall be implemented
+
     public static void run( Activity callingActivity){
         if( isSpeechRecognitionActivityPresented(callingActivity)){
             startRecognitionActivity(callingActivity);
         }else{
             Toast.makeText(callingActivity, "We will have to install Google Speech Search in order to use voice recognition", Toast.LENGTH_LONG).show();
-
+            installGoogleVoiceSearch(callingActivity);
         }
     }
 
+    private static void installGoogleVoiceSearch(final Activity ownerActivity) {
+
+    }
 
 
     private static void startRecognitionActivity(Activity callingActivity) {
