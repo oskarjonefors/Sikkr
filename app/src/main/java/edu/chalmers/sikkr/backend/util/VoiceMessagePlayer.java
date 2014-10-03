de2146a18163bacb2364b83f77d063969f90acde
Oskar Jönefors <oskar.jonefors@gmail.com>
2014-10-03 11:21:38 +0200

    Added record test button in contact view.

--- app/src/main/java/edu/chalmers/sikkr/backend/util/VoiceMessagePlayer.java
+++ app/src/main/java/edu/chalmers/sikkr/backend/util/VoiceMessagePlayer.java
@@ -49,7 +49,6 @@ public class VoiceMessagePlayer {
         try {
             Log.d(TAG, "Trying to play message " + msg.getFileUri());
             player.setDataSource(context, msg.getFileUri());
-            Log.d(TAG, )
             player.prepare();
             player.start();
         } catch (IOException e) {
@@ -60,7 +59,7 @@ public class VoiceMessagePlayer {
     public void stop() {
         if(player != null) {
             player.stop();
-            Log.d(TAG, "Stopping playback.")
+            Log.d(TAG, "Stopping playback.");
         }
     }
 }
