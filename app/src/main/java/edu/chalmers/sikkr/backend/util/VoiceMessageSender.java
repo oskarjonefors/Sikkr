edca124a61c9ce7967f90a29120eede8969f4bc9
Oskar Jönefors <oskar.jonefors@gmail.com>
2014-10-07 22:26:43 +0200

    VoiceMessageSender now works properly. MMS settings are hard-coded for now.

--- app/src/main/java/edu/chalmers/sikkr/backend/util/VoiceMessageSender.java
+++ app/src/main/java/edu/chalmers/sikkr/backend/util/VoiceMessageSender.java
@@ -1,14 +1,25 @@
 package edu.chalmers.sikkr.backend.util;
 
 import android.content.Context;
-import android.content.Intent;
+import android.util.Log;
+
+import com.klinker.android.send_message.Message;
+import com.klinker.android.send_message.Settings;
+import com.klinker.android.send_message.Transaction;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 import edu.chalmers.sikkr.backend.MessageNotSentException;
 import edu.chalmers.sikkr.backend.VoiceMessage;
-import edu.chalmers.sikkr.backend.contact.Contact;
 
 /**
- * Created by ivaldi on 2014-10-01.
+ * A class for sending voice messages as MMS messages. Utilizes the android-smsmms library:
+ * https://github.com/klinker41/android-smsmms
+ *
+ * @author Oskar Jönefors
  */
 
 public class VoiceMessageSender {
@@ -16,12 +27,30 @@ public class VoiceMessageSender {
     private final String TAG = "VoiceMessageSender";
     private final static VoiceMessageSender singleton = new VoiceMessageSender();
     private Context context;
-    private Contact receiver;
+    private Settings mmsSettings;
 
     private VoiceMessageSender() {}
 
     private void setup(Context context) {
         this.context = context;
+
+        /* Configure MMS settings - hard-coded for now */
+        mmsSettings = new Settings();
+        mmsSettings.setMmsc("http://mmsc.tele2.se");
+        mmsSettings.setProxy("130.244.202.30");
+        mmsSettings.setPort("8080");
+    }
+
+    private byte[] getBytes(InputStream inputStream) throws IOException {
+        ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
+        int bufferSize = 1024;
+        byte[] buffer = new byte[bufferSize];
+
+        int len = 0;
+        while ((len = inputStream.read(buffer)) != -1) {
+            byteBuffer.write(buffer, 0, len);
+        }
+        return byteBuffer.toByteArray();
     }
 
     public static VoiceMessageSender getSharedInstance() {
@@ -41,16 +70,25 @@ public class VoiceMessageSender {
 
     /**
      * Send the given VoiceMessage to the given number.
-     * @param msg - The VoiceMessage to send.
+     * @param vmsg - The VoiceMessage to send.
      * @param receiverNbr - The number to send the voice message to.
      * @throws MessageNotSentException - If the message could not be sent.
      * @throws IllegalArgumentException - If the receiver is not a valid one.
      */
-    public void sendMessage(VoiceMessage msg, String receiverNbr) throws MessageNotSentException, IllegalArgumentException {
-        Intent send = new Intent(Intent.ACTION_SEND);
-        send.putExtra("address", receiverNbr);
-        send.putExtra(Intent.EXTRA_STREAM, msg.getFileUri());
-        send.setType("audio/3gp");
-        context.startActivity(send);
+    public void sendMessage(VoiceMessage vmsg, String receiverNbr) throws MessageNotSentException, IllegalArgumentException {
+        final Transaction sendTransaction = new Transaction(context, mmsSettings);
+        final Message msg = new Message("Sikkr message", MessageUtils.fixNumber(receiverNbr));
+
+        try {
+            final InputStream stream = context.getContentResolver().openInputStream(vmsg.getFileUri());
+            msg.setMedia(getBytes(stream), "audio/3gp");
+            msg.setType(Message.TYPE_SMSMMS);
+        } catch (FileNotFoundException e) {
+            Log.e(TAG, "Audio file " + vmsg.getFileUri() + " not found.");
+        } catch (IOException e) {
+            Log.e(TAG, "Couldn't read audio file for sending mms.");
+        }
+        Log.d(TAG, "Sending message to " + msg.getAddresses()[0] + " with thread id " + MessageUtils.getMessageThreadIdByContactId(context, receiverNbr));
+        sendTransaction.sendNewMessage(msg, MessageUtils.getMessageThreadIdByContactId(context, receiverNbr));
     }
 }
