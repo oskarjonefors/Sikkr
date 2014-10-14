cfa75930fcba364714aa612bc712d4ac87cdfec9
Emilia Nilsson <eminil@student.chalmers.se>
2014-10-14 11:33:24 +0200

    Made the callog skip adding a call if the date from when it was called exceeds a month

--- app/src/main/java/edu/chalmers/sikkr/backend/calls/OneCall.java
+++ app/src/main/java/edu/chalmers/sikkr/backend/calls/OneCall.java
@@ -4,14 +4,10 @@ package edu.chalmers.sikkr.backend.calls;
  * Created by Mia on 02/10/14.
  */
 public class OneCall {
-    String callNumber, callName, callDate, isCallNew, callType, contactID;
+    String callNumber, callDate, isCallNew, callType, contactID;
 
     public void setCallNumber(String callNumber) {
         this.callNumber = callNumber;
-    }
-
-    public void setCallName(String callName) {
-        this.callName = callName;
     }
 
     public void setCallDate(String callDate) {
@@ -22,18 +18,12 @@ public class OneCall {
         this.isCallNew = isCallNew;
     }
 
-    public void setCallType(String callType) {
-        this.callType = callType;
-    }
+    public void setCallType(String callType) { this.callType = callType; }
 
     public void setContactID(String contactID) { this.contactID = contactID; }
 
     public String getCallNumber() {
         return callNumber;
-    }
-
-    public String getCallName() {
-        return callName;
     }
 
     public String getCallDate() {
