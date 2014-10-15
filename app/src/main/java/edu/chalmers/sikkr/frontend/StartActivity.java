51a192dabf62e0947b87d203f53bb5dc6ef60c1f
Oskar Jönefors <oskar.jonefors@gmail.com>
2014-10-15 14:21:31 +0200

    Removed unnecessary methods from StartActivity.

--- app/src/main/java/edu/chalmers/sikkr/frontend/StartActivity.java
+++ app/src/main/java/edu/chalmers/sikkr/frontend/StartActivity.java
@@ -48,22 +48,6 @@ public class StartActivity extends Activity {
         new Initializer().execute(this);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.start, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return item.getItemId() == R.id.action_settings || super.onOptionsItemSelected(item);
-    }
-
     /**
      * Actionhandler for this activity
      *
