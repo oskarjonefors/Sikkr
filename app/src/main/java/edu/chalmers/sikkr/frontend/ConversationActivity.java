92c104ba5af29dbe0bb395735345f4f290d00579
Jesper Olsson <jespols@student.chalmers.se>
2014-10-22 16:19:37 +0200

    Removed some unused code

--- app/src/main/java/edu/chalmers/sikkr/frontend/ConversationActivity.java
+++ app/src/main/java/edu/chalmers/sikkr/frontend/ConversationActivity.java
@@ -112,8 +112,7 @@ public class ConversationActivity extends Activity {
 
     public void createConversationLayout(){
         final Bundle bundle = getIntent().getExtras();
-        if(bundle!=null && bundle.containsKey("position") && bundle.containsKey("name")){
-            //thisConversation = SMS_Activity.getConversations().get(bundle.getInt("position"));
+        if(bundle!=null && bundle.containsKey("number") && bundle.containsKey("name")){
             thisConversation = TheInbox.getInstance().getConversation(bundle.getString("number"));
             TextView tv = (TextView)findViewById(R.id.conversation_name);
             tv.setText(bundle.getString("name"));
