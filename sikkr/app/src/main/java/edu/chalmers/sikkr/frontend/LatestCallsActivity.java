d5f2b1b8ed91bb501e87671f4c2a213721527a20
Emilia Nilsson <eminil@student.chalmers.se>
2014-10-31 15:55:27 +0100

    Added comments to the LatestCallsActivity class

--- sikkr/app/src/main/java/edu/chalmers/sikkr/frontend/LatestCallsActivity.java
+++ sikkr/app/src/main/java/edu/chalmers/sikkr/frontend/LatestCallsActivity.java
@@ -49,9 +49,32 @@ public class LatestCallsActivity extends Activity {
         createCallLogLayout();
     }
 
+    /**
+     * Takes the call list from the CallLog class and sorts them by date and refines the list.
+     * Creates the list view layout with each item in the list connected to one call.
+     */
+    private void createCallLogLayout() {
+        List<OneCall> callList = CallLog.getInstance().getCallList();
+        Collections.sort(callList);
 
-    private List<OneCall> createRefinedList (List<OneCall> callList){
-        Map <String, OneCall > map = new LinkedHashMap<>();
+        List<OneCall> refinedList = this.createRefinedList(callList);
+
+        ArrayAdapter adapter = new LatestCallItemAdapter(this, refinedList);
+        ListView listV = (ListView) findViewById(R.id.listView);
+        listV.setAdapter(adapter);
+    }
+
+    /**
+     * Refines the call list so that calls that were created from the same person and with the same
+     * type get sorted as only one call which shows the amount of times it was made. For example
+     * if there exist two calls after each other from the same phone number that are all outgoing,
+     * then it will be merged as one, showing the number of times it was made and which type it was.
+     * @param callList The list that contains all the calls.
+     * @return A refined list where calls that are of the same number and that were made after each
+     * other gets merged into one call and then setting an amount of them.
+     */
+    private List<OneCall> createRefinedList (List<OneCall> callList) {
+        Map<String, OneCall> map = new LinkedHashMap<>();
         OneCall iCall, jCall;
         String iCallNumber, jCallNumber;
         int iCallType, jCallType;
@@ -79,21 +102,7 @@ public class LatestCallsActivity extends Activity {
                 }
             }
         }
-
         return new ArrayList<>(map.values());
-
-    }
-
-    private void createCallLogLayout() {
-
-        List<OneCall> callList = CallLog.getInstance().getCallList();
-        Collections.sort(callList);
-
-        List<OneCall> refinedList = this.createRefinedList(callList);
-
-        ArrayAdapter adapter = new LatestCallItemAdapter(this, refinedList);
-        ListView listV = (ListView) findViewById(R.id.listView);
-        listV.setAdapter(adapter);
     }
 
     private static class ViewHolder {
@@ -124,6 +133,11 @@ public class LatestCallsActivity extends Activity {
             Resources res = context.getResources();
             Contact contact;
 
+            /*
+             * If there exists no view, we create one and save all the elements from the view
+             * (the view is the latest_call_item) that we need to edit later. Else we reuse the
+             * view (this happens when scrolling through the list and scroll back again).
+             */
             if (view == null) {
                 LayoutInflater inflater = ((Activity) context).getLayoutInflater();
                 view = inflater.inflate(R.layout.latest_call_item, viewGroup, false);
@@ -137,12 +151,23 @@ public class LatestCallsActivity extends Activity {
                 holder = (ViewHolder) view.getTag();
             }
 
+            /*
+             * Sets the name text to "private number" if the number is private (all private numbers
+             * starts with "-". Else, the number exists and we write either the number itself or
+             * the name the number is connected to.
+             */
             if(list.get(i).getCallNumber().startsWith("-")) {
                 holder.name.setText("Private number");
             } else {
                 holder.name.setText(getContactNameByNbr(MessageUtils.fixNumber(list.get(i).getCallNumber())));
             }
 
+            /* Gets the contact by it's name, and if there is no name (i.e only a phone number) then
+             * it will return null. This is so we can set the photo if there exists one. Otherwise,
+             * everything is set to null to avoid the view to reuse information. Setting listeners
+             * where we send either the contact or contact number makes it possible to click on
+             * a item in the list and either go to contact information or call the number directly.
+             */
             contact = ContactBook.getSharedInstance().getFirstContactByName((holder.name.getText() + "").trim());
             if (contact != null) {
                 holder.contact = contact;
@@ -161,10 +186,18 @@ public class LatestCallsActivity extends Activity {
                 view.setOnClickListener(new ContactGridClickListener(list.get(i).getCallNumber()));
             }
 
+            /*
+             * First we get the difference in date from today and from when the call was made. Then
+             * we write the amount of times the call was made if it is bigger than 2 times, and
+             * then we write how long ago the call was made.
+             */
             String callDate = DateDiffUtility.callDateToString(Long.parseLong( list.get(i).getCallDate()), LatestCallsActivity.this);
             holder.callTypeAmountAndDate.setText
                     (list.get(i).getCallTypeAmount()<2 ? "" + callDate : "(" + list.get(i).getCallTypeAmount() + ")" + "\n" +  callDate);
 
+            /*
+             * Here we set the picture accordingly to the call type.
+             */
             switch (list.get(i).getCallType()) {
                 case android.provider.CallLog.Calls.INCOMING_TYPE:
                     holder.bitmap = BitmapFactory.decodeResource(res, res.getIdentifier("incoming_call", "drawable", context.getPackageName()));
@@ -189,15 +222,16 @@ public class LatestCallsActivity extends Activity {
                     holder.image.setImageDrawable(holder.drawable);
                     holder.name.setTextColor(getResources().getColor(android.R.color.holo_red_light));
                     break;
-
             }
             return view;
         }
 
         /**
-         * Get the saved contact name related to the number from the phonebook
-         * @param number the number to get the contact name from
-         * @return the name of the contact
+         * Get the saved contact name related to the number from the phonebook, if there is no
+         * contact related to the number (i.e it's a unsaved phone number), then it will return
+         * the number itself.
+         * @param number The number to get the contact name from.
+         * @return The name of the contact or the number if there exist no name.
          */
         public String getContactNameByNbr(String number) {
             String contact = "";
