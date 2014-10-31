e95facaa53f6977562088d78970570f04f678ed8
Oskar Jönefors <oskar.jonefors@gmail.com>
2014-10-31 14:50:25 +0100

    Localized strings in SpeechRecognitionHelper.

--- sikkr/app/src/main/java/edu/chalmers/sikkr/backend/util/SpeechRecognitionHelper.java
+++ sikkr/app/src/main/java/edu/chalmers/sikkr/backend/util/SpeechRecognitionHelper.java
@@ -7,9 +7,10 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.speech.RecognizerIntent;
-import android.widget.Toast;
 
 import java.util.List;
+
+import edu.chalmers.sikkr.R;
 
 /**
  * A class to handle the speech recognition feature.
@@ -27,7 +28,6 @@ public class SpeechRecognitionHelper{
         if( isSpeechRecognitionActivityPresented(callingActivity)){
             startRecognitionActivity(callingActivity);
         }else{
-            Toast.makeText(callingActivity, "We will have to install Google Speech Search in order to use voice recognition", Toast.LENGTH_LONG).show();
             installGoogleVoiceSearch(callingActivity);
         }
     }
@@ -37,9 +37,10 @@ public class SpeechRecognitionHelper{
      * @param ownerActivity - the activity calling this method.
      */
     private static void installGoogleVoiceSearch(final Activity ownerActivity) {
-        AlertDialog dialog  = new AlertDialog.Builder(ownerActivity).setMessage("It's necessary to install Google Voice Search").setTitle(
-                "Install Google Voice Search from Google Play?")
-                .setPositiveButton("Install", new DialogInterface.OnClickListener() {
+        AlertDialog dialog  = new AlertDialog.Builder(ownerActivity).setMessage(
+                ownerActivity.getString(R.string.no_voice_search)).setTitle(
+                ownerActivity.getString(R.string.install_voice_question))
+                .setPositiveButton(ownerActivity.getString(R.string.install), new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         try {
                             Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.google.android.voicesearch"));
@@ -50,7 +51,7 @@ public class SpeechRecognitionHelper{
                         }
                     }
                 })
-                    .setNegativeButton("Cancel", null)
+                    .setNegativeButton(ownerActivity.getString(R.string.cancel), null)
                     .create();
 
         dialog.show();
@@ -62,7 +63,7 @@ public class SpeechRecognitionHelper{
      */
     private static void startRecognitionActivity(Activity callingActivity) {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Select an application");
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, callingActivity.getString(R.string.say_command));
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
         intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS,1);
 
